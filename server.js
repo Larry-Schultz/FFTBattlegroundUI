9208887374030706b001be7ec18da445dd2bcb03
Other Brand <larryw.schultz@gmail.com>
2022-06-29 16:58:52 -0500

    fixed "Not Found" issue

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fftb-viewer-ui'));
+app.use(express.static(__dirname + '/dist/FFTBattlegroundUI'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fftb-viewer-ui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/FFTBattlegroundUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
